ac4d9ba14247df424d103a24a11c190e78421cb2
Usama00777 <admin@keshmart.com>
2025-04-16 11:30:28 +0500

    Add files via upload

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('WinMoxx backend running');
-});
+app.get('/', (req, res) => res.send('WinMoxx backend working!'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
